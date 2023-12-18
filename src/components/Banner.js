64f0f96bde380c71ec9aac20fa19b75e86fac696
Yassine MIDI <yassinemidi1@gmail.com>
2023-12-18 11:51:22 +0100

    link social media

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -16,7 +16,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "FullStack Developer", "Salesforce Developer", "Vlocity Developer", "Software engineer" ];
+  const toRotate = ["FullStack Developer", "Salesforce Developer", "Vlocity Developer", "Software engineer"];
   const period = 2000;
 
   useEffect(() => {
@@ -54,54 +54,54 @@ export const Banner = () => {
 
   return (
     <Router>
-    <section className="banner" id="home">
-      <Container>
-        <Row className="aligh-items-center">
-          <Col xs={12} md={6} xl={7}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
-                <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Yassine MIDI`} <br/> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> */}
-                  {/* begining */}
-                  <>
-      
+      <section className="banner" id="home">
+        <Container>
+          <Row className="aligh-items-center">
+            <Col xs={12} md={6} xl={7}>
+              <TrackVisibility>
+                {({ isVisible }) =>
+                  <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                    <span className="tagline">Welcome to my Portfolio</span>
+                    <h1>{`Hi! I'm Yassine MIDI`} <br /> <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
+                    {/* <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p> */}
+                    {/* begining */}
+                    <>
 
-      <p>Bonjour et bienvenue sur mon portfolio ! Je suis Yassine MIDI, un développeur Salesforce passionné par la création de solutions innovantes et efficaces. Titulaire d'un diplôme d'ingénieur d'État en Génie Informatique de l'ENSA Safi, je me spécialise dans le développement Salesforce avec une solide expertise dans diverses technologies et outils associés à cette plateforme.</p>
 
-      <h2>À propos de moi</h2>
+                      <p>Bonjour et bienvenue sur mon portfolio ! Je suis Yassine MIDI, un développeur Salesforce passionné par la création de solutions innovantes et efficaces. Titulaire d'un diplôme d'ingénieur d'État en Génie Informatique de l'ENSA Safi, je me spécialise dans le développement Salesforce avec une solide expertise dans diverses technologies et outils associés à cette plateforme.</p>
 
-      <p>Je suis certifié Salesforce Administrateur, Salesforce Développeur 1 et Salesforce associé. Actuellement en cours de préparation pour la certification Omnistudio, je suis constamment en quête d'élargir mes connaissances pour rester à la pointe des dernières avancées technologiques.</p>
+                      <h2>À propos de moi</h2>
 
-     
+                      <p>Je suis certifié Salesforce Administrateur, Salesforce Développeur 1 et Salesforce associé. Actuellement en cours de préparation pour la certification Omnistudio, je suis constamment en quête d'élargir mes connaissances pour rester à la pointe des dernières avancées technologiques.</p>
 
-      
 
-      <p>N'hésitez pas à explorer mon portfolio pour découvrir certains de mes projets et réalisations. Je suis ouvert aux opportunités de collaboration et de nouveaux défis. Si vous partagez une passion pour l'innovation et l'excellence, je serais ravi de discuter de la manière dont nous pourrions travailler ensemble.
-      </p>
-    </>
-                  {/* end */}
-                  
-                  
-                  <HashLink to='#connect'>
-                  <button className="vvd" onClick={() => console.log('connect')}><span>Let’s Connect</span> <ArrowRightCircle size={25} /></button>
-                  </HashLink>
-                  
-              </div>}
-            </TrackVisibility>
-          </Col>
-          <Col xs={12} md={6} xl={5}>
-            <TrackVisibility>
-              {({ isVisible }) =>
-                <div className={isVisible ? "animate__animated animate__backInRight" : ""}>
-                  <img src={headerImg} alt="Header Img"/>
-                </div>}
-            </TrackVisibility>
-          </Col>
-        </Row>
-      </Container>
-    </section>
+
+
+
+                      <p>N'hésitez pas à explorer mon portfolio pour découvrir certains de mes projets et réalisations. Je suis ouvert aux opportunités de collaboration et de nouveaux défis. Si vous partagez une passion pour l'innovation et l'excellence, je serais ravi de discuter de la manière dont nous pourrions travailler ensemble.
+                      </p>
+                    </>
+                    {/* end */}
+
+
+                    <HashLink to='#connect'>
+                      <button className="vvd" onClick={() => console.log('connect')}><span>Let’s Connect</span> <ArrowRightCircle size={25} /></button>
+                    </HashLink>
+
+                  </div>}
+              </TrackVisibility>
+            </Col>
+            <Col xs={12} md={6} xl={5}>
+              <TrackVisibility>
+                {({ isVisible }) =>
+                  <div className={isVisible ? "animate__animated animate__backInRight" : ""}>
+                    <img src={headerImg} alt="Header Img" />
+                  </div>}
+              </TrackVisibility>
+            </Col>
+          </Row>
+        </Container>
+      </section>
     </Router>
   )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,9 +16,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+                <a href="https://www.linkedin.com/in/yassine-midi-5b942b145/"><img src={navIcon1} alt="" /></a>
+                <a href="https://github.com/yassinemidi"><img src={navIcon2} alt="" /></a>
             </div>
             <p>Yassine MIDI</p>
           </Col>
